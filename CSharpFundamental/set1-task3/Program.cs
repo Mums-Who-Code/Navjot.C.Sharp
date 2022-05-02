831493c32e3e6f2365d6742a4f968746a3b01fd4
navjot kaur <kaurnavjot1983@outlook.com>
2022-05-02 14:12:01 -0700

    CSHARPFUNDAMENTAL:Set1-Task3

---
+++ CSharpFundamental/set1-task3/Program.cs
@@ -0,0 +1,4 @@
+﻿string name = "navjot";
+DateTime date = new DateTime(2021, 12, 24);
+Console.WriteLine("\t My name is " + name + ".");
+Console.WriteLine("\t I joined MumsWhoCode on " + date.ToString("d") + ".");
--- CSharpFundamentals/Set1-Task3/Set1-Task3/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿string name = "navjot";
-DateTime date = new DateTime(2021,12,24);
-Console.WriteLine("\t My name is " +name+".");
-Console.WriteLine("\t I joined MumsWhoCode on "+ date.ToString("d")+".");
