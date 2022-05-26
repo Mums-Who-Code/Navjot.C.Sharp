efeeec4293e46a96c157aeb61917ca9fc4424980
navjot kaur <kaurnavjot1983@outlook.com>
2022-05-26 11:10:33 -0700

    CSHARPFUNDAMENTAL: Set1-Task5

---
+++ CSharpFundamentals/Set1-Task2/Set1-Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿Random number = new Random();
+int randomNumber = number.Next(1,7);
+Console.WriteLine("\n The random number is: = "+ randomNumber);
+
+DateTime currentDate = DateTime.Now;
+Console.WriteLine("\n Todays date is := "+ currentDate.ToString());
+ 
+DateTime addnumber = currentDate.AddDays(randomNumber);
+Console.WriteLine("\n Added a random number to today's date := "+addnumber.ToString());
+Console.ReadLine();
